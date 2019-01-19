1a193d268b298c217a216935c759f87673518182
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2019-01-19 09:33:54 +0100

    Guess encoding for argparse file input

--- CorrectOCR/__main__.py
+++ CorrectOCR/__main__.py
@@ -15,6 +15,21 @@ if __name__=='__main__':
 	import configparser
 	import argparse
 	
+	import chardet
+	
+	def get_encoding(file):
+		with open(file, 'rb') as f:
+			detection = chardet.detect(f.read(1024*50))
+			return detection['encoding']
+	
+	class FileType(argparse.FileType):
+		def __call__(self, string):
+			if self._mode == 'r' and string != '-':
+				self._encoding = get_encoding(string)
+				return super().__call__(string)
+			else:
+				return super().__call__(string)
+	
 	config = configparser.RawConfigParser()
 	config.optionxform = lambda option: option
 	config.read_string(defaults)
@@ -27,12 +42,12 @@ if __name__=='__main__':
 	subparsers = mainparser.add_subparsers(dest='command', help='Choose command')
 	
 	dictparser = subparsers.add_parser('build_dictionary', help='Build dictionary')
-	dictparser.add_argument('output', type=argparse.FileType('w', encoding='utf-8'))
+	dictparser.add_argument('output', type=FileType('w', encoding='utf-8'))
 	dictparser.add_argument('files', nargs='*')
 	dictparser.set_defaults(func=dictionary.build_dictionary)
 	
 	alignparser = subparsers.add_parser('align', help='Create alignments')
-	alignparser.add_argument('--filepair', action='append', nargs=2, dest='filepairs', type=argparse.FileType('r', encoding='Windows-1252')) # TODO guess encoding?
+	alignparser.add_argument('--filepair', action='append', nargs=2, dest='filepairs', type=FileType('r'))
 	alignparser.set_defaults(func=model.align_pairs)
 	
 	alignparser = subparsers.add_parser('build_model', help='Build model')
