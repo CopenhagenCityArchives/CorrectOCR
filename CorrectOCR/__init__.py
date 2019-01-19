cd8275f4fbb7e8b027e69395c60da77139c18a15
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2019-01-19 14:05:23 +0100

    get_encoding: use beautifulsoup’s UnicodeDammit instead of chardet

--- CorrectOCR/__init__.py
+++ CorrectOCR/__init__.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
-import chardet
+import os
+from bs4 import UnicodeDammit
+import logging
 
 def get_encoding(file):
 	with open(file, 'rb') as f:
-		detection = chardet.detect(f.read(1024*50))
-		return detection['encoding']
+		dammit = UnicodeDammit(f.read(1024*500), ['utf-8', 'Windows-1252'])
+		logging.getLogger(__name__+'.get_encoding').debug('detecting %s for %s' % (dammit.original_encoding, file))
+		return dammit.original_encoding
+
+def clean(settings):
+	os.remove(settings.hmmParams)
+	#TODO
