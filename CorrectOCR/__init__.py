8331a26681fd3003d64faec416ac38544e65dd8f
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2019-01-22 15:38:47 +0100

    __init__ cleanup

--- CorrectOCR/__init__.py
+++ CorrectOCR/__init__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 import os
-from bs4 import UnicodeDammit
 import logging
+from bs4 import UnicodeDammit
 from collections import deque
 
 def get_encoding(file):
 	with open(file, 'rb') as f:
 		dammit = UnicodeDammit(f.read(1024*500), ['utf-8', 'Windows-1252'])
-		logging.getLogger(__name__+'.get_encoding').debug('detecting %s for %s' % (dammit.original_encoding, file))
+		logging.getLogger(__name__+'.get_encoding').debug('detected %s for %s' % (dammit.original_encoding, file))
 		return dammit.original_encoding
 
 def open_for_reading(file):
