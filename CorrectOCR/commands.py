05518ddcc15f68f0847733e17eaee1e758bb33be
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2019-02-13 20:04:09 +0100

    Dictionary corpus update (289,416 words)

--- CorrectOCR/commands.py
+++ CorrectOCR/commands.py
@@ -3,10 +3,15 @@ import logging
 import random
 import shutil
 import time
+import zipfile
+from pathlib import Path
 
+import lxml
 import progressbar
+import requests
+from tei_reader import TeiReader
 
-from . import ensure_new_file
+from . import ensure_new_file, open_for_reading, extract_text_from_pdf
 from .dictionary import Dictionary
 from .aligner import Aligner
 from .model import HMM, HMMBuilder
@@ -20,38 +25,146 @@ def build_dictionary(workspace, config):
 	log = logging.getLogger(f'{__name__}.build_dictionary')
 	
 	if config.corpusFile:
-		for line in config.corpusFile.readlines():
+		for line in open_for_reading(config.corpusFile).readlines():
 			line = line.strip()
 			if len(line) == 0:
 				pass
 			elif line[0] == '#':
 				log.info(line)
 			elif line[:4] == 'http':
-				outfile = config.corpusPath.joinpath(Path(line).name)
-				if outfile.exists():
+				if '\t' in line:
+					(url, filename) = line.split('\t')
+					filename = config.corpusPath.joinpath(Path(filename).name)
+				else:
+					url = line
+					filename = config.corpusPath.joinpath(Path(line).name)
+				if filename.exists():
 					log.info('Download cached, will not download again.')
 					continue
-				r = requests.get(line)
+				r = requests.get(url)
 				if r.status_code == 200:
-					with open(outfile, 'wb') as f:
+					with open(filename, 'wb') as f:
 						f.write(r.content)
 				else:
 					log.error(f'Unable to save file: {r}')
 				time.sleep(random.uniform(0.5, 1.5))
 			elif line[-1] == '/':
 				for file in Path(line).iterdir():
-					outfile = config.corpusPath.joinpath(Path(line).name)
+					outfile = config.corpusPath.joinpath(file.name)
 					if outfile.exists():
-						log.info(f'File already copied: {file}')
+						log.info(f'File already copied: {file.name}')
 						continue
-					log.info(f'Copying {file} to corpus.')
+					log.info(f'Copying {file.name} to corpus.')
 					shutil.copy(file, outfile)
-	
+
+	def unzip_recursive(zip):
+		for member in zip.namelist():
+			if member[-4:] == '.zip':
+				log.info(f'Unzipping internal {member}')
+				with zipfile.ZipFile(zip.open(member)) as zf:
+					unzip_recursive(zf)
+			else:
+				zip.extract(member, config.corpusPath)
+
 	for file in config.corpusPath.iterdir():
+		if file.suffix == '.zip':
+			log.info(f'Unzipping {file}')
+			with zipfile.ZipFile(file) as zf:
+				unzip_recursive(zf)
+
+	ignore = {
+		'.DS_Store',
+		# extraneous files in Joh. V. Jensen zip:
+		'teiHeader.xsd',
+		'text-format.pdf',
+		'text-header.pdf',
+		# invalid XML files in Grundtvig zip:
+		'1816_297_4_txt.xml',
+		'1816_297_7_txt.xml',
+		'1816_297_11_txt.xml',
+		'1817_306_12_txt.xml',
+		'1817_306_13_txt.xml',
+		'1817_306_14Txt2Tei.xml',
+		'1817_306_15_txt.xml',
+		'1817_306_16_txt.xml',
+		'1817_309A_txt.xml',
+		'1817_310_txt.xml',
+		'1818_336A_1_txt.xml',
+		'1819_342_1.txt.xml',
+		'1819_342_2_txt.xml',
+		'1819_342_3_txt.xml',
+		'1819_342_txt.xml',
+		'1825_410_1_txt.xml',
+		'1825_411_1_txt.xml',
+		'1826_444_2_txt.xml',
+		'1826_444_3_txt.xml',
+		'1826_444_4_txt.xml',
+		'1826_444_5_txt.xml',
+		'1826_450_1_txt.xml',
+		'1826_450_2_txt.xml',
+		'1826_459_1_txt.xml',
+		'1827_456_1_txt.xml',
+		'1827_456_2_txt.xml',
+		'1827_459_1_txt.xml',
+		'1827_459_2_txt.xml',
+		'1831_490A_txt.xml',
+		'1831_495A_txt.xml',
+		'1834_534A_txt.xml',
+		'1836_566_txt.xml',
+		'1838_601_txt.xml',
+		'1839_617_1_txt.xml',
+		'1839_617_3.xml',
+		'1839_639_txt.xml',
+		'1839_641_txt.xml',
+		'1840_652_txt.xml',
+		'1840_656_txt.xml',
+		'1849_946_txt.xml',
+		'1849_966_txt.xml',
+		'1853_1034_txt.xml',
+		'1853_1035_txt.xml',
+		'1853_1036_txt.xml',
+		'1853_1041.xml',
+		'1855_1123_txt.xml',
+		'1855_1126a_txt.xml',
+		'1855_1126b_txt.xml',
+		'1855_1126c_txt.xml',
+		'1855_1126d_txt.xml',
+		'1855_1126e_txt.xml',
+		'1855_1126f_txt.xml',
+		'1855_1126g_txt.xml',
+		'1855_1129_txt.xml',
+		'1856_1135_txt.xml',
+		'1856_1138_txt.xml',
+		'1858_1229_txt.xml',
+		'1859_1242_txt.xml',
+		'1859_1254_txt.xml',
+		'1861_1283.xml',
+		'1864_1365_txt.xml',
+		'1866_1394_txt.xml',
+		'1866_1396_txt.xml',
+	}
+
+	for file in config.corpusPath.glob('**/*'):
+		if file.name[0] == '.' or file.name in ignore:
+			continue
 		log.info(f'Getting words from {file}')
 		if file.suffix == '.pdf':
 			text = extract_text_from_pdf(file)
 			for word in tokenize_string(str(text), config.language.name, objectify=False):
+				workspace.resources.dictionary.add(word)
+		elif file.suffix == '.xml':
+			try:
+				reader = TeiReader()
+				corpora = reader.read_file(file)
+			except lxml.etree.XMLSyntaxError:
+				log.error(f'XML error in {file}')
+				continue
+			# approved = {'corpus', 'document', 'div', 'part', 'p', 'l', 'w'}
+			# text = corpora.tostring(lambda e, t: t if e.tag in approved else '')
+			# above didn't work. Instead insert extra space, see issue
+			# https://github.com/UUDigitalHumanitieslab/tei_reader/issues/6
+			text = corpora.tostring(lambda e, t: f'{t} ')
+			for word in tokenize_string(text, config.language.name, objectify=False):
 				workspace.resources.dictionary.add(word)
 		elif file.suffix == '.txt':
 			with open_for_reading(file) as f:
--- CorrectOCR/dictionary.py
+++ CorrectOCR/dictionary.py
@@ -5,18 +5,19 @@ from . import open_for_reading, ensure_new_file, extract_text_from_pdf
 
 
 class Dictionary(object):
-	def __init__(self, file=None, caseInsensitive=False):
+	def __init__(self, path=None, caseInsensitive=False):
 		self.log = logging.getLogger(f'{__name__}.Dictionary')
 		self.caseInsensitive = caseInsensitive
 		self.words = set()
-		self.file = file
-		if Path(self.file.name).exists():
-			self.log.info(f'Loading dictionary from {self.file.name}')
-			for line in self.file.readlines():
-				if self.caseInsensitive:
-					self.words.add(line.strip().lower())
-				else:
-					self.words.add(line.strip())
+		self.path = path
+		if self.path.exists():
+			self.log.info(f'Loading dictionary from {self.path.name}')
+			with open_for_reading(self.path) as f:
+				for line in f.readlines():
+					if self.caseInsensitive:
+						self.words.add(line.strip().lower())
+					else:
+						self.words.add(line.strip())
 		self.log.info(f'{len(self.words)} words in dictionary')
 	
 	def __str__(self):
@@ -50,11 +51,10 @@ class Dictionary(object):
 		self.words.add(word)
 	
 	def save(self, path=None):
-		self.file.close()
-		name = path or self.file.name
-		newname = ensure_new_file(Path(name))
-		self.log.info(f'Backed up original dictionary file to {newname}')
-		self.log.info(f'Saving dictionary (words: {len(self.words)}) to {name}')
-		with open(name, 'w', encoding='utf-8') as f:
+		path = path or self.path
+		backup = ensure_new_file(path)
+		self.log.info(f'Backed up original dictionary file to {backup}')
+		self.log.info(f'Saving dictionary (words: {len(self.words)}) to {path}')
+		with open(path, 'w', encoding='utf-8') as f:
 			for word in sorted(self.words, key=str.lower):
 				f.write(f'{word}\n')
