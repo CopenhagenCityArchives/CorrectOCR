5a065ce080b61f3d24b5a08dd6f3b52b6b5df7c4
Jens Feodor Nielsen <jensfeodor@gmail.com>
2019-05-03 12:45:27 +0200

    Use file_id/file_index as key for token database table.

--- initdb.d/dbinit.sql
+++ initdb.d/dbinit.sql
@@ -1,5 +1,6 @@
 CREATE TABLE token (
-	id INT NOT NULL AUTO_INCREMENT,
+	file_id VARCHAR(255) NOT NULL,
+	file_index INT NOT NULL,
 	original VARCHAR(255) NOT NULL,
 	gold VARCHAR(255),
 	bin INT,
@@ -8,17 +9,16 @@ CREATE TABLE token (
 	selection VARCHAR(255),
 	token_type VARCHAR(255),
 	token_info TEXT,
-	file_id VARCHAR(255) NOT NULL,
-	file_index INT NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY (file_id, file_index)
 );
 
 CREATE TABLE kbest (
 	id INT NOT NULL AUTO_INCREMENT,
-	token_id INT NOT NULL,
+	file_id VARCHAR(255) NOT NULL,
+	file_index INT NOT NULL,
 	k INT NOT NULL,
 	candidate VARCHAR(255) NOT NULL,
 	probability float NOT NULL,
 	PRIMARY KEY (id),
-	CONSTRAINT FOREIGN KEY fk_kbest_token (token_id) REFERENCES token(id)
+	CONSTRAINT FOREIGN KEY fk_kbest_token(file_id, file_index) REFERENCES token(file_id, file_index)
 );
