fe29e3d0d44123f2e5f246b3564dbe104b8fe3e8
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2019-12-13 11:42:19 +0100

    mock doc package requirement (pyodbc)

--- docs/conf.py
+++ docs/conf.py
@@ -190,7 +190,7 @@ autodoc_default_options = {
 }
 autodoc_member_order = 'bysource'
 
-autodoc_mock_imports = ['tesserocr']
+autodoc_mock_imports = ['pyodbc', 'tesserocr']
 
 autoclass_content = 'both'
 
