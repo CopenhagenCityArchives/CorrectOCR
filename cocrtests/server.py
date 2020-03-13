308b9ab846adec5fbd5530f77d3527f0d30ffac6
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2020-03-13 12:36:33 +0100

    Unit tests: server avoid spurious debug logging

--- cocrtests/server.py
+++ cocrtests/server.py
@@ -27,70 +27,71 @@ class ServerTests(unittest.TestCase):
 		)
 		self.config = MockConfig(k=4)
 
-		self.app = create_app(self.workspace, self.config).test_client()
+		self.app = create_app(self.workspace, self.config)
+		self.app.logger.setLevel(logging.getLogger().getEffectiveLevel())
+		self.client = self.app.test_client()
 
 	def test_index(self):
-		response = self.app.get('/', follow_redirects=True)
+		response = self.client.get('/', follow_redirects=True)
 		self.assertEqual(len(response.json), len(self.workspace.docids_for_ext('.pdf')))
 		self.assertEqual(response.json[0]['count'], 5, f'Incorrect response: {response.json}')
 		self.assertEqual(response.json[0]['corrected'], 1, f'Incorrect response: {response.json}')
 
 	def test_doc_view(self):
-		response = self.app.get('/abc/tokens.json', follow_redirects=True)
+		response = self.client.get('/abc/tokens.json', follow_redirects=True)
 		self.assertEqual(len(response.json), 5, f'Incorrect response: {response.json}')
 		self.assertTrue(response.json[0]['is_corrected'], f'Incorrect response: {response.json}')
 		self.assertFalse(response.json[1]['is_corrected'], f'Incorrect response: {response.json}')
 
 	def test_token_view(self):
-		response = self.app.get('/abc/token-0.json', follow_redirects=True)
+		response = self.client.get('/abc/token-0.json', follow_redirects=True)
 		logging.debug(f'response: {response.json}')
 		self.assertEqual(response.json['Original'], 'Once', f'Incorrect response: {response.json}')
 		self.assertEqual(response.json['Gold'], 'Once', f'Incorrect response: {response.json}')
 
-		response = self.app.get('/abc/token-1.json', follow_redirects=True)
+		response = self.client.get('/abc/token-1.json', follow_redirects=True)
 		self.assertEqual(response.json['Original'], 'upen', f'Incorrect response: {response.json}')
 		self.assertEqual(response.json['Gold'], '', f'Incorrect response: {response.json}')
 
 	def test_token_update(self):
-		response = self.app.get('/', follow_redirects=True)
+		response = self.client.get('/', follow_redirects=True)
 		self.assertEqual(response.json[0]['corrected'], 1, f'Incorrect response: {response.json}')
 	
-		response = self.app.get('/abc/token-1.json', follow_redirects=True)
+		response = self.client.get('/abc/token-1.json', follow_redirects=True)
 		self.assertEqual(response.json['Original'], 'upen', f'Incorrect response: {response.json}')
 		self.assertEqual(response.json['Gold'], '', f'Incorrect response: {response.json}')
 
-		response = self.app.post('/abc/token-1.json', data={'gold': 'upon'}, follow_redirects=True)
+		response = self.client.post('/abc/token-1.json', data={'gold': 'upon'}, follow_redirects=True)
 		self.assertEqual(response.json['Original'], 'upen', f'Incorrect response: {response.json}')
 		self.assertEqual(response.json['Gold'], 'upon', f'Incorrect response: {response.json}')
 
-		response = self.app.get('/', follow_redirects=True)
+		response = self.client.get('/', follow_redirects=True)
 		self.assertEqual(response.json[0]['corrected'], 2, f'Incorrect response: {response.json}')
 
 	def test_token_hyphenate_left(self):
-		response = self.app.get('/abc/token-3.json', follow_redirects=True)
+		response = self.client.get('/abc/token-3.json', follow_redirects=True)
 		self.assertFalse(response.json['Hyphenated'], f'Incorrect response: {response.json}')
-		response = self.app.get('/abc/token-4.json', follow_redirects=True)
+		response = self.client.get('/abc/token-4.json', follow_redirects=True)
 		self.assertFalse(response.json['Hyphenated'], f'Incorrect response: {response.json}')
 
-		response = self.app.post('/abc/token-4.json', data={'hyphenate': 'left'}, follow_redirects=True)
+		response = self.client.post('/abc/token-4.json', data={'hyphenate': 'left'}, follow_redirects=True)
 		self.assertFalse(response.json['Hyphenated'], f'Incorrect response: {response.json}')
-		response = self.app.get('/abc/token-3.json', follow_redirects=True)
+		response = self.client.get('/abc/token-3.json', follow_redirects=True)
 		self.assertTrue(response.json['Hyphenated'], f'Incorrect response: {response.json}')
 
 	def test_token_hyphenate_right(self):
-		response = self.app.get('/abc/token-3.json', follow_redirects=True)
+		response = self.client.get('/abc/token-3.json', follow_redirects=True)
 		self.assertFalse(response.json['Hyphenated'], f'Incorrect response: {response.json}')
 
-		response = self.app.post('/abc/token-3.json', data={'hyphenate': 'right'}, follow_redirects=True)
+		response = self.client.post('/abc/token-3.json', data={'hyphenate': 'right'}, follow_redirects=True)
 		self.assertTrue(response.json['Hyphenated'], f'Incorrect response: {response.json}')
 	
 	def test_random(self):
-		response = self.app.get('/random', follow_redirects=False)
+		response = self.client.get('/random', follow_redirects=False)
 		self.assertEqual(response.status_code, 302)
 		
 		location_matcher = re.compile(r'^http://localhost/([^/]+)/token-(\d+)\.json$')
 		self.assertTrue(location_matcher.match(response.location), f'{location_matcher} should match {response.location}')
 		
-		response = self.app.get(response.location, follow_redirects=False)
+		response = self.client.get(response.location, follow_redirects=False)
 		self.assertEqual(response.status_code, 200)
-		
