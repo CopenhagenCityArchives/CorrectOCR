23aa9bc30b3c9bbcd5468d065d199dc1a7e02302
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2020-11-23 11:26:20 +0100

    Unit test: heuristics

--- cocrtests/__init__.py
+++ cocrtests/__init__.py
@@ -3,6 +3,7 @@ import logging
 logging.disable(logging.WARN)
 logging.debug(f'If this text is visible, debug logging is active. Change it in {__file__}')
 
+from .heuristics import *
 from .hyphenation import *
 from .pdf import *
 from .punctuation import *
---
+++ cocrtests/heuristics.py
@@ -0,0 +1,32 @@
+import unittest
+from unittest.mock import mock_open
+
+import logging
+import sys
+
+from .mocks import *
+
+from CorrectOCR.tokens import Tokenizer, Token, KBestItem
+from CorrectOCR.heuristics import Heuristics
+
+class TestHeuristics(unittest.TestCase):
+	def test_auto_dehyphenation(self):
+		t = Tokenizer.for_extension('.txt')(language=MockLang('english'), dehyphenate=True)
+
+		f = MockCorpusFile('String')
+		tokens = t.tokenize(f, MockConfig(type='fs'))
+		token = tokens[0]
+		
+		token.kbest = {
+			1: KBestItem("String", 1.0),
+		}
+
+		dictionary = set(["String"])
+		settings = {
+			1: "o",
+		}
+		heuristics = Heuristics(settings, dictionary)
+		
+		heuristics.bin_tokens(tokens)
+
+		self.assertEqual(token.bin.number, 1, f'Token should be in bin 1.')
