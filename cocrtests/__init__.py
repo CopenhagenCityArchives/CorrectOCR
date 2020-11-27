e68fb0a177fd3c963f402c8d3a18e4cd09d9011e
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2020-11-27 10:56:39 +0100

    Unit test: model

--- cocrtests/__init__.py
+++ cocrtests/__init__.py
@@ -5,6 +5,7 @@ logging.debug(f'If this text is visible, debug logging is active. Change it in {
 
 from .heuristics import *
 from .hyphenation import *
+from .model import *
 from .pdf import *
 from .punctuation import *
 from .server import *
