b89126cce1739b5418388370678af4b3a13d5643
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2021-02-11 14:29:47 +0100

    Util: hyphenRE to match more hyphen types

--- CorrectOCR/_util.py
+++ CorrectOCR/_util.py
@@ -2,6 +2,8 @@ import regex
 
 punctuationRE = regex.compile(r'\p{punct}+')
 
+hyphenRE = regex.compile('[\{Pd}\xad]+$')
+
 _punctuation_splitter = regex.compile(r'^(\p{punct}*)(.*?)(\p{punct}*)$')
 
 def punctuation_splitter(s):
