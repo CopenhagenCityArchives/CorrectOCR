b476c680c60963dcc364de2ef584b48fd6ce739b
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2021-03-18 10:41:28 +0100

    hyphenRE: also match regular -

--- CorrectOCR/_util.py
+++ CorrectOCR/_util.py
@@ -2,7 +2,7 @@ import regex
 
 punctuationRE = regex.compile(r'\p{punct}+')
 
-hyphenRE = regex.compile('[\{Pd}\xad]+$')
+hyphenRE = regex.compile('[\{Pd}\xad\-]+$')
 
 _punctuation_splitter = regex.compile(r'^(\p{punct}*)(.*?)(\p{punct}*)$')
 
