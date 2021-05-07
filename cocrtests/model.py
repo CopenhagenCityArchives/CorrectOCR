aea64b957fa5983682a391fca1c06b7fd32f0c2b
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2021-05-07 12:08:13 +0200

    Unit tests (model): kbest for hyphenated tokens

--- cocrtests/model.py
+++ cocrtests/model.py
@@ -18,7 +18,7 @@ class TestModel(unittest.TestCase):
 
 		readCounts = {
 			"S": { "S": 1000},
-			"t": { "l": 1000},
+			"t": { "t": 999, "l": 1},
 			"r": { "r": 1000},
 			"i": { "i": 1000},
 			"n": { "n": 1000},
@@ -36,3 +36,35 @@ class TestModel(unittest.TestCase):
 		hmm.generate_kbest(tokens)
 
 		self.assertEqual(token.kbest[1].candidate, 'String', f'The first candidate should be "String".')
+
+	def test_hyphenated_kbest(self):
+		t = Tokenizer.for_extension('.txt')(language=MockLang('english'))
+
+		f = MockCorpusFile('Slr- ing')
+		tokens = t.tokenize(f, MockConfig(type='fs'))
+		tokens.dehyphenate()
+		token = tokens[0]
+
+		dictionary = set(["String"])
+
+		readCounts = {
+			"S": { "S": 1000},
+			"t": { "t": 999, "l": 1},
+			"r": { "r": 1000},
+			"i": { "i": 1000},
+			"n": { "n": 1000},
+			"g": { "g": 1000},
+			"-": { "-": 1000},
+		}
+		gold_words = ["String"]
+
+		builder = HMMBuilder(dictionary, 0.0001, 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', readCounts, [], gold_words)
+
+		hmm = HMM(None, None, use_cache=False)
+		hmm.init = builder.init
+		hmm.tran = builder.tran
+		hmm.emis = builder.emis
+
+		hmm.generate_kbest(tokens)
+
+		self.assertEqual(token.kbest[1].candidate, 'Str-ing', f'The first candidate should be "String".')
