bb416e4791b35a23b87adfe33a4db7a0f8cccc9d
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2021-06-03 16:29:28 +0200

    Unit Test (aligner): test apply_as_gold

--- cocrtests/aligner.py
+++ cocrtests/aligner.py
@@ -7,7 +7,7 @@ from CorrectOCR.tokens import Tokenizer
 
 
 class TestAligner(unittest.TestCase):
-	def test_aligner(self):
+	def test_alignments(self):
 		t = Tokenizer.for_extension('.txt')(language=MockLang('english'))
 
 		f = MockCorpusFile('This is a t3st')
@@ -27,3 +27,16 @@ class TestAligner(unittest.TestCase):
 		# defaultdict(<class 'dict'>, {'This': {0: 'This'}, 'is': {1: 'is'}, 'a': {2: 'a'}, 't3st': {3: 'test'}})
 		# defaultdict(<class 'collections.Counter'>, {'T': Counter({'T': 1}), 'h': Counter({'h': 1}), 'i': Counter({'i': 2}), 's': Counter({'s': 3}), 'a': Counter({'a': 1}), 't': Counter({'t': 2}), '3': Counter({'e': 1})})
 		self.assertEqual(readCounts['3']['e'], 1, f'The character 3 should have been read as e exactly once: {readCounts}')
+
+	def test_align_gold(self):
+		t = Tokenizer.for_extension('.txt')(language=MockLang('english'))
+
+		left = t.tokenize(MockCorpusFile('This is a t3st'), MockConfig(type='fs'))
+		right = t.tokenize(MockCorpusFile('This is a test'), MockConfig(type='fs'))
+		
+		aligner = Aligner()
+		
+		aligner.apply_as_gold(left, right)
+
+		for l, r in zip(left, right):
+			self.assertEqual(l.gold, r.original, f'{l} should have gold from {r}')
