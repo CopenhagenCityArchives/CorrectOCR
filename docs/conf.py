d0ce6cddfe553fa83c889a1c500bbd6bad30efa9
Mikkel Eide Eriksen <mikkel.eriksen@gmail.com>
2021-06-25 10:38:26 +0200

    Docs: set workspace root

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+os.environ['CORRECTOCR_WORKSPACE_ROOTPATH'] = '../workspace'
+
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 
 
